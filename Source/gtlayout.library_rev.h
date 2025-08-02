15b9473c2b5365f35e9dddfe86684a61faf450f6
amigazen <amigazen@github.com>
2025-08-02 22:48:10 +0100

    Initial commit of original gtlayout.library retrieved from Aminet https://www.aminet.net/package/dev/gui/gtlayout 2-Aug-2025

---
+++ Source/gtlayout.library_rev.h
@@ -0,0 +1,6 @@
+#define VERSION		47
+#define REVISION	2
+#define DATE		"3.1.2012"
+#define VERS		"gtlayout.library 47.2"
+#define VSTRING		"gtlayout.library 47.2 (3.1.2012)\r\n"
+#define VERSTAG		"\0$VER: gtlayout.library 47.2 (3.1.2012)"
